f3f9fe24b5e0d66a64e26c6df1877a26e53ee583
Cipher-Coder <matthew.rebehn@gmail.com>
2019-03-02 16:09:56 -0500

    starting on new word puzzle

--- src/components/wordsearch.vue
+++ src/components/wordsearch.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-   
+    <h1 class="f1 blue tc">Welcome to {{ welcome }}</h1>
   </div>
 </template>
 
@@ -9,56 +9,14 @@ export default {
   name: "wordsearch",
   data: function() {
     return {
-      welcome: "Aiden's Word Search"
+      welcome: "Aiden's Word Search!"
     };
   }
 };
 </script>
 
 <style scoped>
-tr,
-td {
-  padding: 5px;
-}
-
-.tablist {
-  border: 1px solid black;
-  border-bottom: 0px;
-  margin-left: 5px;
-  padding-left: 5px;
-  padding-right: 5px;
-}
-
-.tabcontent {
-  padding: 5px;
-}
-
-.inputTitle {
-  font-weight: bold;
-  font-size: 12px;
-}
-
-#explainWords {
-  font-size: x-small;
-}
-
-#menuButton {
-  background-color: #555555;
-  color: white;
-  display: block;
-  width: 60px;
-  text-align: center;
-  cursor: pointer;
-  height: 20px;
-  line-height: 20px;
-  text-decoration: none;
-}
-
-#title {
-  color: blue;
-}
-
-#create {
-  margin-bottom: 80px;
+h1 {
+  padding: 30px;
 }
 </style>
