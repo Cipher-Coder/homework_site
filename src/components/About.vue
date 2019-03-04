1bd5370bbcdd98b7b84642f421f5746bc90f29d9
Cipher-Coder <matthew.rebehn@gmail.com>
2019-03-04 09:34:13 -0500

    updated and styled vocab words

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="about">
-    <h1 class="f1">This is the Vocabulary Page</h1>
-    <h2 class="f2">Vocabulary words for this week:</h2>
+    <h1 class="f1">Word Lists</h1>
+    <h2 class="f2">Vocabulary words for this week: "Homophones"</h2>
+    <p class="f7 red">* High Frequency Words</p>
     <div class="pa4">
       <div class="overflow-auto">
         <table class="f6 w-100 mw8 center" cellspacing="0">
@@ -31,58 +32,74 @@
               <td class="pa3">through</td>
             </tr>
             <tr class="stripe-dark">
-              <td class="pa3">boy</td>
-              <td class="pa3">does</td>
-              <td class="pa3">everyone</td>
-              <td class="pa3">field</td>
+              <td class="pa3 red">boy</td>
+              <td class="pa3 red">does</td>
+              <td class="pa3 red">everyone</td>
+              <td class="pa3 red">field</td>
             </tr>
             <tr class="stripe-dark">
-              <td class="pa3">floor</td>
-              <td class="pa3">found</td>
-              <td class="pa3">into</td>
-              <td class="pa3">their</td>
+              <td class="pa3 red">floor</td>
+              <td class="pa3 red">found</td>
+              <td class="pa3 red">into</td>
+              <td class="pa3 red">their</td>
             </tr>
+            <tr class="stripe-dark">
+              <td class="pa3"></td>
+              <td class="pa3 red">toward</td>
+              <td class="pa3 red">what's</td>
+              <td class="pa3"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="pa4">
+      <div class="overflow-auto">
+        <h2 class="f2 pb2">Vocabulary words for next week: Suffixes -y, -ly, -ful</h2>
+        <p class="f7 red pb4">* High Frequency Words</p>
+        <table class="f6 w-100 mw8 center" cellspacing="0">
+          <tbody class="lh-copy">
             <tr class="stripe-dark">
               <td class="pa3">helpful</td>
-              <td class="pa3">toward</td>
-              <td class="pa3">what's</td>
               <td class="pa3">sadly</td>
-            </tr>
-            <tr class="stripe-dark">
               <td class="pa3">hopeful</td>
               <td class="pa3">thankful</td>
+            </tr>
+            <tr class="stripe-dark">
               <td class="pa3">slowly</td>
               <td class="pa3">wishful</td>
-            </tr>
-            <tr class="stripe-dark">
               <td class="pa3">kindly</td>
               <td class="pa3">useful</td>
+            </tr>
+            <tr class="stripe-dark">
               <td class="pa3">safely</td>
               <td class="pa3">painful</td>
-            </tr>
-            <tr class="stripe-dark">
               <td class="pa3">mouthful</td>
               <td class="pa3">weakly</td>
+            </tr>
+            <tr class="stripe-dark">
               <td class="pa3">jumped</td>
               <td class="pa3">saying</td>
-            </tr>
-            <tr class="stripe-dark">
               <td class="pa3">quickly</td>
               <td class="pa3">wonderful</td>
-              <td class="pa3">coming</td>
-              <td class="pa3">down</td>
             </tr>
             <tr class="stripe-dark">
-              <td class="pa3">four</td>
-              <td class="pa3">give</td>
-              <td class="pa3">great</td>
-              <td class="pa3">idea</td>
+              <td class="pa3 red">coming</td>
+              <td class="pa3 red">down</td>
+              <td class="pa3 red">four</td>
+              <td class="pa3 red">give</td>
             </tr>
             <tr class="stripe-dark">
-              <td class="pa3">knew</td>
-              <td class="pa3">large</td>
-              <td class="pa3">though</td>
-              <td class="pa3">write</td>
+              <td class="pa3 red">great</td>
+              <td class="pa3 red">idea</td>
+              <td class="pa3 red">knew</td>
+              <td class="pa3 red">large</td>
+            </tr>
+            <tr class="stripe-dark">
+              <td class="pa3"></td>
+              <td class="pa3 red">though</td>
+              <td class="pa3 red">write</td>
+              <td class="pa3"></td>
             </tr>
           </tbody>
         </table>
